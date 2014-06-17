c8670bacc56f3e46651b29ca80dc30b8520dee44
Aidan Delaney <aidan@ontologyengineering.org>
2014-06-17 19:59:26 +0100

    Example level using melonJS

---
+++ src/js/resources.js
@@ -0,0 +1,11 @@
+/**
+ * ressources
+ */
+
+var global_resources= [
+    // images
+    {name: "free_tileset_version_10",   type:"image",       src: "data/free_tileset_version_10.png"},
+
+    // game level
+    {name: "village",   type: "tmx",  src: "data/village.tmx"}
+];
